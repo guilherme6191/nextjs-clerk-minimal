94e5c7bb6093670206afec6a0032bf5a0c26ca46
guilherme6191 <guilherme@clerk.dev>
2025-05-16 17:51:37 -0300

    sub layout fix

---
+++ src/app/(in-app)/layout.tsx
@@ -0,0 +1,29 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      {children}
+    </ClerkProvider>
+  );
+}
